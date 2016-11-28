5338c8ce4234cb128206ea9b7e9f225ee571c238
thorirv15 <thorirv15@ru.is>
2016-11-28 13:36:53 +0000

    fyrsta drasl

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "hundur" << endl;
+
+   for(int i = 0; i < 5; i++)
+    {
+       cout << "kottur" << endl;
+   }
+    return 0;
+}
