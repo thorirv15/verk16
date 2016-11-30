b445957099768ae04b8cfa6ea2214bd3b2603fbf
hermanno16 <hermanno16@ru.is>
2016-11-30 10:55:27 +0000

    Smávægilegar lagfæringar

--- ComputerScientists/dataaccess.h
+++ ComputerScientists/dataaccess.h
@@ -14,14 +14,8 @@ public:
 
     DataAccess();
 
-    void fromFileToClass ( string inputFirstName , string inputSecondName , string inputGender , string inputBirthyear , string inputDeceased )
-    {
-        _firstName   = inputFirstName;       // Nálgumst private breyturnar hérna
-        _lastName    = inputSecondName;
-        _gender      = inputGender;
-        _dateOfBirth = inputBirthyear;
-        _deceased    = inputDeceased;
-    }
+    void fromFileToClass ( string inputFirstName , string inputSecondName , string inputGender , string inputBirthyear , string inputDeceased );
+
 
 private:
 
@@ -35,33 +29,7 @@ private:
 
 vector<DataAccess> listOfScientists;  // Vektorinn sem inniheldur klasana með upplýsingunum
 
-void dataFromFile( vector<DataAccess> listOfScientists )
-{
-    ifstream theFile;
+void dataFromFile( vector<DataAccess> listOfScientists );
 
-    DataAccess tempClass; // Temp klasi til að setja í vektorinn í enda while lykkjunnar
-
-    string inputFirstName;
-    string inputSecondName;
-    string inputGender;
-    string inputBirthyear;
-    string inputDeceased;
-    char stop;
-
-    theFile.open( "scientists.txt" );
-
-    while ( theFile >> inputFirstName >> inputSecondName >> inputGender >> inputBirthyear >> inputDeceased >> stop )
-    {
-        tempClass.fromFileToClass ( inputFirstName , inputSecondName , inputGender , inputBirthyear , inputDeceased )
-        if ( stop == '#' )
-        {
-            listOfScientists.push_back( temp );
-        }
-
-    }
-
-    theFile.close();
-
-}
 
 #endif // DATAACCESS_H
