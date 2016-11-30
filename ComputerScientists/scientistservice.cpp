7a21baf397f0e62f7abe18c4d17735577953da3b
hermanno16 <hermanno16@ru.is>
2016-11-30 17:16:49 +0000

    boolean operators

--- ComputerScientists/scientistservice.cpp
+++ ComputerScientists/scientistservice.cpp
@@ -1,9 +1,51 @@
 #include "scientistservice.h"
+#include "scientist.h"
+
+
+
 
 ScientistService::ScientistService()
 {
 
 }
 
-//1.Fall sem lætur dataaccess ná í einhverjar upplýsingar úr skránni
-//2.
+int ScientistService::getName
+{
+    return _name;
+}
+
+int ScientistService::getGender
+{
+    return _gender;
+}
+
+int ScientistService::getDoB
+{
+    return _dateOfBirth;
+}
+
+int ScientistService::getDeath
+{
+    return _deceased;
+}
+
+
+bool SortingFunctions::sortNameAsc ( const ScientistService& lhs, const ScientistService& rhs )
+{
+    return lhs.getName() < rhs.getName();
+}
+
+bool SortingFunctions::sortNameDesc ( const ScientistService& lhs, const ScientistService& rhs )
+{
+    return rhs.getName() < lhs.getName();
+}
+
+void SortingFunctions::alphabetAsc ( vector<ScientistService> allScientists )
+{
+    sort ( allScientists.begin() , allScientists.end() , sortNameAsc )
+}
+
+void SortingFunctions::alphabetDesc ( vector<ScientistService> allScientists )
+{
+    sort ( allScientists.begin() , allScientists.end() , sortNameDesc )
+}
--- ComputerScientists/scientistservice.h
+++ ComputerScientists/scientistservice.h
@@ -1,27 +1,48 @@
 #ifndef SCIENTISTSERVICE_H
 #define SCIENTISTSERVICE_H
+
 #include <string>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
 
 
 class ScientistService
 {
 public:
+
     ScientistService();
 
-    int findScientist(string name)
-    {
-
-
-    }
-
-
-
-
+    int getName;
+    int getGender;
+    int getDoB;
+    int getDeath;
 
 private:
 
+    string _name;
+    string _gender;
+    string _dateOfBirth;
+    string _deceased;
+
 };
+
+struct SortingFunctions
+{
+    bool sortNameAsc ( const ScientistService& lhs, const ScientistService& rhs );
+    bool sortNameDesc ( const ScientistService& lhs, const ScientistService& rhs );
+
+    void alphabetAsc ( vector<ScientistService> allScientists );
+    void alphabetDesc ( vector<ScientistService> allScientists );
+
+};
+
+vector<ScientistService> allScientists;
+vector<ScientistService> inputQuery;
+
+
+
+
 
 #endif // SCIENTISTSERVICE_H
